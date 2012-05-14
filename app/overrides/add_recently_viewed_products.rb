e8c19b5ee4ca846a4dd43252ed18286735316259
Ben Eggett <beneggett@gmail.com>
2012-05-14 06:07:17 -0600

    Removed products hook. (cherry picked from commit c7b3fb584fbec1c0c6787d9d74ff9bdaf4624ead)

--- app/overrides/add_recently_viewed_products.rb
+++ app/overrides/add_recently_viewed_products.rb
@@ -1,7 +1,7 @@
-#Deface::Override.new(:virtual_path => 'spree/shared/_products',
-                     #:name => 'add_recently_viewed_products_to_products_index',
-                     #:insert_after => ".recently-viewed-block[data-hook]",
-                     #:partial => 'spree/shared/recently_viewed_products')
+# Deface::Override.new(:virtual_path => 'spree/shared/_products',
+#                      :name => 'add_recently_viewed_products_to_products_index',
+#                      :insert_after => "#products[data-hook], [data-hook='products']",
+#                      :partial => 'spree/shared/recently_viewed_products')
 
 Deface::Override.new(:virtual_path => 'spree/products/_taxons',
                      :name => 'add_recently_viewed_products_to_products_taxons',
