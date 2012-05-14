56cc134d71faee258b3f42622c1a066f057dc599
Ben Eggett <beneggett@gmail.com>
2012-05-14 08:59:35 -0600

    Changed hook to go to bottom of related-items div. (cherry picked from commit 1b4101180a7bc9a440bf016e1305d73870958376)
    Signed-off-by: Tobias Bohwalli <hi@futhr.io>

--- app/overrides/add_recently_viewed_products.rb
+++ app/overrides/add_recently_viewed_products.rb
@@ -3,7 +3,7 @@
 #                      :insert_after => "#products[data-hook], [data-hook='products']",
 #                      :partial => 'spree/shared/recently_viewed_products')
 
-Deface::Override.new(:virtual_path => 'spree/products/_taxons',
+Deface::Override.new(:virtual_path => 'spree/products/show',
                      :name => 'add_recently_viewed_products_to_products_taxons',
-                     :insert_after => "#taxon-crumbs[data-hook]",
+                     :insert_bottom => "#related-items[data-hook], [data-hook='related-items']",
                      :partial => 'spree/shared/recently_viewed_products')
