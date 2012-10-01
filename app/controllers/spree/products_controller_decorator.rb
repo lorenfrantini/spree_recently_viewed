5ecd21ef9d95873a45364d6d09f55bc5d755b9e6
Andrew Haust <andrewwhhaust@gmail.com>
2012-10-01 14:01:55 -0400

    Including BaseHlper in product_controller

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,4 +1,6 @@
 Spree::ProductsController.class_eval do
+  include ::Spree::BaseHelper
+
   after_filter :recently_viewed, :only => :show
 
   def recently_viewed
@@ -10,14 +12,6 @@ Spree::ProductsController.class_eval do
     rvp_max_count = Spree::RecentlyViewed::Config[:recently_viewed_products_max_count] || 5
     rvp.delete_at(0) if rvp.size > rvp_max_count.to_i
     session['recently_viewed_products'] = rvp.join(',')
-
-    #id = @product.id
-    #rvp = session.fetch('recently_viewed_products', '').split(', ')
-    #rvp.delete(id)
-    #rvp << id unless rvp.include?(id.to_s)
-    #rvp_max_count = Spree::RecentlyViewed::Config[:recently_viewed_products_max_count] || 5
-    #rvp.delete_at(0) if rvp.size > rvp_max_count.to_i
-    #session['recently_viewed_products'] = rvp.join(', ')
   end
 
 end
