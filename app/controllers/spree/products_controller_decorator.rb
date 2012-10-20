bcd5f92fcb598e0a565547e9a3c7befc1d4a8986
Andrew Haust <andrewwhhaust@gmail.com>
2012-10-19 20:41:37 -0400

    Remove product if viewing a product variant
    In GelaSkins speak: if you have recenly viewed an artwork then look at a
    wrap of this artwork, the artwork is removed from recently viewed
    leaving just the wrap.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -8,6 +8,7 @@ Spree::ProductsController.class_eval do
     rvp = session.fetch('recently_viewed_products', [])
     rvp = rvp.split(',') unless rvp.is_a? Array
     rvp.delete(item)
+    rvp.delete('Spree::Product-' + @product.product_id.to_s) if @product.is_a? Spree::Variant
     rvp << item unless rvp.include?(item)
     rvp_max_count = Spree::RecentlyViewed::Config[:recently_viewed_products_max_count] || 5
     rvp.delete_at(0) if rvp.size > rvp_max_count.to_i
